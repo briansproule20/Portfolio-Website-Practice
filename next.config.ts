c8a089bf0571ca6eadbc9d7e3deec534ca4b3aa6
Brian Sproule <briansproule20@gmail.com>
2025-06-01 18:36:53 -0400

    Added Google Sheets private key handling in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }
 };
 
 export default nextConfig; 
